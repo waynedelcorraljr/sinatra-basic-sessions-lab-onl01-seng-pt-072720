b0d9687c3357cf01b94f4f098fdf84864192abd1
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-10-25 13:07:32 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "lucca"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @cart_contents = session[:item]
+        erb :checkout
+    end
+
 end
